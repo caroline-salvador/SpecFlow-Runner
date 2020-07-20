14230573c2a8ad4d2519f15812771c2444f240b1
Caroline Salvador <caroline.farias.salvador@gmail.com>
2020-07-20 14:04:05 -0300

    Resumo incluído

--- TestProject/Report/Report.cs
+++ TestProject/Report/Report.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver.Core.Operations;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using MongoDB.Driver.Core.Operations;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -11,11 +12,16 @@ namespace TestProject.Report
     static class ManualTestReport
     {
         private static StringBuilder html_buffer = new StringBuilder();
+        private static StringBuilder html_summary = new StringBuilder();
+
         private static string title_feature = null;
         private static int contp = 0;
         private static int contf = 0;
 
-        private static string align = "right";
+        private static string align_right = "right";
+        private static string align_left = "left";
+        private static string background = "#DCDCDC";
+        private static string style_table = "width: 100%";
 
         public static void InsertReportData(string feature, string scenario, string tag)
         {
@@ -23,46 +29,32 @@ namespace TestProject.Report
             {
                 if (title_feature != null)
                 {
-                    html_buffer.Append(@"<tr>");
-                    html_buffer.Append(@"<th>Succeeded</th>");
-                    html_buffer.Append(@"<th>Failed</th>");
-                    html_buffer.Append(@"</tr>");
-
-                    /*html_buffer.Append("<style>");
-                    html_buffer.Append("tr {");
-                    html_buffer.Append("text - align:right;");
-                    html_buffer.Append("background-color:#c0c4c1 }");
-                    html_buffer.Append("</style>");*/
-
-                    html_buffer.Append($"<tr align={align}>");
-                    html_buffer.Append($"<td>{contp}</td>");
-                    html_buffer.Append($"<td>{contf}</td>");
-                    html_buffer.Append(@"</tr>");
+                    html_summary.Append($"<tr>");
+                    html_summary.Append($"<td>{title_feature}</td>");
+                    html_summary.Append($"<td align={align_right}>{contp + contf}</td>");
+                    html_summary.Append($"<td align={align_right}>{contp}</td>");
+                    html_summary.Append($"<td align={align_right}>{contf}</td>");
+                    html_summary.Append(@"</tr>");
 
                     html_buffer.Append(@"</table>");
                 }
 
                 //header
-                html_buffer.Append($"<H3> FEATURE: {feature} </H3>");
+                html_buffer.Append($"<H3> FUNCIONALIDADE: {feature} </H3>");
 
                 //style
                 html_buffer.Append(@"<style>");
                 html_buffer.Append(@"table, th, td {");
                 html_buffer.Append(@"border: 1px solid black;");
                 html_buffer.Append(@"border-collapse: collapse; }");
-                html_buffer.Append(@"table {");
-                html_buffer.Append(@"width: 100 %; }");
                 html_buffer.Append(@"</style>");
 
                 //table
-                html_buffer.Append(@"<table>");
+                html_buffer.Append($"<table style={style_table}>");
                 html_buffer.Append(@"<tr>");
-                html_buffer.Append(@"<th>Scenario</th>");
-                html_buffer.Append(@"<th>Status</th>");
+                html_buffer.Append($"<th bgcolor={background}>Cenário</th>");
+                html_buffer.Append($"<th bgcolor={background} >Estado</th>");
 
-                /*html_buffer.Append(@"<th>Succeeded</th>");
-                html_buffer.Append(@"<th>Failed</th>");
-                html_buffer.Append(@"<th>Blocked</th>");*/
                 html_buffer.Append(@"</tr>");
 
                 contp = 0;
@@ -91,48 +83,38 @@ namespace TestProject.Report
             html_buffer.Append(@"<tr>");
             html_buffer.Append($"<td> <font color={scenario_color}> {scenario} </font> </td>");
             html_buffer.Append($"<td> <b> <font color={tag_color}> {tag.ToUpper()} </font> </b> </td>");
-
-            /*if (tag == "passou")
-            {
-                scenario_color = "green";
-                contp++;
-                html_buffer.Append($"<td> <b> <font color={scenario_color}> {tag.ToUpper()} </font> </b> </td>"); //passou
-                html_buffer.Append($"<td></td>"); //falhou
-                html_buffer.Append($"<td></td>"); //bloqueado
-            }
-            else if (tag == "falhou")
-            {
-                scenario_color = "red";
-                contf++;
-                html_buffer.Append($"<td></td>"); //passou
-                html_buffer.Append($"<td> <b> <font color={scenario_color}> {tag.ToUpper()} </font> </b> </td>"); //falhou
-                html_buffer.Append($"<td></td>"); //bloqueado
-            }
-            else if (tag == "bloqueado")
-            {
-                scenario_color = "purple";
-                html_buffer.Append($"<td></td>"); //passou
-                html_buffer.Append($"<td></td>"); //falhou
-                html_buffer.Append($"<td> <b> <font color={scenario_color}> {tag.ToUpper()} </font> </b> </td>"); //bloqueado
-            }*/
-            
             html_buffer.Append(@"</tr>");
         }
 
         public static string GetReport()
         {
-            html_buffer.Append(@"<tr>");
-            html_buffer.Append(@"<th>Succeeded</th>");
-            html_buffer.Append(@"<th>Failed</th>");
-            html_buffer.Append(@"</tr>");
-
-            html_buffer.Append($"<tr align={align}>");
-            html_buffer.Append($"<td>{contp}</td>");
-            html_buffer.Append($"<td>{contf}</td>");
-            html_buffer.Append(@"</tr>");
+            html_summary.Append($"<tr>");
+            html_summary.Append($"<td>{title_feature}</td>");
+            html_summary.Append($"<td align={align_right}>{contp + contf}</td>");
+            html_summary.Append($"<td align={align_right}>{contp}</td>");
+            html_summary.Append($"<td align={align_right}>{contf}</td>");
+            html_summary.Append(@"</tr>");
+            html_summary.Append(@"</table>");
 
             html_buffer.Append(@"</table>");
-            return html_buffer.ToString();
+            
+            StringBuilder html = new StringBuilder();
+
+            html.Append(@"<H3> RESUMO </H3>");
+            html.Append(@"<style>");
+            html.Append(@"table, th, td {");
+            html.Append(@"border: 1px solid black;");
+            html.Append(@"border-collapse: collapse; }");
+            html.Append(@"</style>");
+            html.Append($"<table style={style_table}>");
+            html.Append(@"<tr>");
+            html.Append($"<th align={align_left}, bgcolor={background}>Funcionalidade</th>");
+            html.Append($"<th bgcolor={background}>Cenários</th>");
+            html.Append($"<th bgcolor={background}>Sucesso</th>");
+            html.Append($"<th bgcolor={background}>Falhou</th>");
+            html.Append(@"</tr>");
+
+            return html.ToString() + html_summary.ToString() + html_buffer.ToString();
         }
     }
 }
